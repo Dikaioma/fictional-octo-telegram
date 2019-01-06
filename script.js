6427a4ec8be35e41aced6ed4cb8f56dbc18cdf3c
Dikaíoma <43892662+Dikaioma@users.noreply.github.com>
2019-01-05 22:53:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 //$('#pizza').click('background-color: yellow');
-$(document).ready(function() {
+/*$(document).ready(function() {
     $('#pizza').click(function() {
         $(this)
         .css('backgroundColor','yellow')
     });
+});*/
+$(document).ready(function(){
+    var myFunction = function() {
+        $('#pizza').css('background-color', 'yellow');   
+    };
 });
